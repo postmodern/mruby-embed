10962ded921b76d92c770d1f8ddd94051bd7a526
Postmodern <postmodern.mod3@gmail.com>
2013-10-25 19:37:39 -0700

    Made a little REPL.

--- lib/main.rb
+++ lib/main.rb
@@ -1,1 +1,11 @@
-puts "hello world"
+loop do
+  print '>> '
+  line = gets.chomp
+  begin
+    result = eval(line)
+
+    puts "=> #{result.inspect}"
+  rescue => error
+    puts error.message
+  end
+end
